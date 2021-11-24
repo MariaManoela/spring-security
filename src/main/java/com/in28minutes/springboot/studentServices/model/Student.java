63177c22dbb3c75e4c214e0ad33620a3192c4d51
Maria Manoela <manoelamvc@hotmail.com>
2021-11-24 10:46:05 -0300

    Updating student and course classes

--- src/main/java/com/in28minutes/springboot/studentServices/model/Student.java
+++ src/main/java/com/in28minutes/springboot/studentServices/model/Student.java
@@ -2,14 +2,24 @@ package com.in28minutes.springboot.studentServices.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
+import javax.websocket.server.ServerEndpoint;
 import java.util.List;
 
 @AllArgsConstructor
 @Getter
+@Setter
 public class Student {
     private String id;
     private String name;
     private String description;
     private List<Course> courses;
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Student [id=%s, name=%s, description=%s, courses=%s]", id,
+                name, description, courses);
+    }
 }
