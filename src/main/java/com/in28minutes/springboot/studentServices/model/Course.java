c7893bb65fe0e3e9d6f50d8fa776e7cea83b4f8c
Maria Manoela <manoelamvc@hotmail.com>
2021-11-24 09:38:27 -0300

    Adding attributes

--- src/main/java/com/in28minutes/springboot/studentServices/model/Course.java
+++ src/main/java/com/in28minutes/springboot/studentServices/model/Course.java
@@ -1,4 +1,10 @@
 package com.in28minutes.springboot.studentServices.model;
 
+import java.util.List;
+
 public class Course {
+    private String id;
+    private String name;
+    private String description;
+    private List<String> steps;
 }
--- src/main/java/com/in28minutes/springboot/studentServices/model/Student.java
+++ src/main/java/com/in28minutes/springboot/studentServices/model/Student.java
@@ -1,4 +1,10 @@
 package com.in28minutes.springboot.studentServices.model;
 
+import java.util.List;
+
 public class Student {
+    private String id;
+    private String name;
+    private String description;
+    private List<Course> courses;
 }
