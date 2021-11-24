bb69038212e6a999cc8a124bcb5367a09973346d
Maria Manoela <manoelamvc@hotmail.com>
2021-11-24 09:44:05 -0300

    Implementing lombok getter and constructor

--- src/main/java/com/in28minutes/springboot/studentServices/model/Course.java
+++ src/main/java/com/in28minutes/springboot/studentServices/model/Course.java
@@ -1,7 +1,10 @@
 package com.in28minutes.springboot.studentServices.model;
 
+import lombok.AllArgsConstructor;
+
 import java.util.List;
 
+@AllArgsConstructor
 public class Course {
     private String id;
     private String name;
--- src/main/java/com/in28minutes/springboot/studentServices/model/Student.java
+++ src/main/java/com/in28minutes/springboot/studentServices/model/Student.java
@@ -1,7 +1,12 @@
 package com.in28minutes.springboot.studentServices.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.List;
 
+@AllArgsConstructor
+@Getter
 public class Student {
     private String id;
     private String name;
