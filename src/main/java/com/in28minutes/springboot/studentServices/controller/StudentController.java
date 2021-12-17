46fe78cf828264b673320e3a26a9cb0f15742cdf
Maria Manoela <manoelamvc@hotmail.com>
2021-12-17 10:18:36 -0300

    Add GET and POST request

--- src/main/java/com/in28minutes/springboot/studentServices/controller/StudentController.java
+++ src/main/java/com/in28minutes/springboot/studentServices/controller/StudentController.java
@@ -3,10 +3,11 @@ package com.in28minutes.springboot.studentServices.controller;
 import com.in28minutes.springboot.studentServices.model.Course;
 import com.in28minutes.springboot.studentServices.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -15,7 +16,27 @@ public class StudentController {
     private StudentService studentService;
 
     @GetMapping("/students/{studentId}/courses")
-    public List<Course> retrieveCoursesForStudent(@PathVariable String studentId){
+    public List<Course> retrieveCoursesForStudent(@PathVariable String studentId) {
         return studentService.retrieveCourses(studentId);
     }
+
+    @GetMapping("/students/{studentId}/courses/{courseId}")
+    public Course retrieveDetailsForCourse(@PathVariable String studentId,
+                                           @PathVariable String courseId) {
+        return studentService.retrieveCourse(studentId, courseId);
+    }
+
+    @PostMapping("/students/{studentId}/courses")
+        public ResponseEntity<Void> registerStudentForCourse(
+                @PathVariable String studentId, @RequestBody Course newCourse){
+            Course course = studentService.addCourse(studentId, newCourse);
+
+            if (course == null)
+                return ResponseEntity.noContent().build();
+
+            URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                    .buildAndExpand(course.getId()).toUri();
+
+            return ResponseEntity.created(location).build();
+        }
 }
